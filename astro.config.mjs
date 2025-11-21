e561cba8099fd62c0eabd293616b8b84294d8586
25JT <jhonierlucumimeza@gmail.com>
2025-11-21 18:19:28 -0500

    we

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,22 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import node from '@astrojs/node'; // Add this line to import the node adapter
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        external: ['@studio-freight/lenis']
+      }
+    }
   }
 });
