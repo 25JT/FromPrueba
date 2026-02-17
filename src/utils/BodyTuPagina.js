ecd68eb197f717df7dba54b70fc5ce3083f53f08
25JT <jhonierlucumimeza@gmail.com>
2026-02-17 10:50:04 -0500

    Actualizacion apra tiempo real de la edicion de la pagina

--- src/utils/BodyTuPagina.js
+++ src/utils/BodyTuPagina.js
@@ -1,7 +1,43 @@
+import { ruta } from "../utils/ruta.js"
+import { alertaMal } from "../assets/Alertas/Alertas";
+
 const tituloTienda = document.getElementById("shop-name");
 const direccionTienda = document.getElementById("shop-address");
 const descripcionTienda = document.getElementById("shop-desc");
-let timer
+//cargar la pagina si la persona tiene negocio como veria la pespectiva si no cargara los default
+window.onload = () => {
+    fetch(`${ruta}/api/tienda/identidad`, {
+        method: "GET",
+        credentials: "include"
+
+    })
+        //si hay data sera enviada al las id
+        .then(response => response.json())
+        .then(data => {
+            if (data.rows[0] === null) {
+                tituloTienda.value = "";
+                direccionTienda.value = "";
+                descripcionTienda.value = "";
+            } else {
+                tituloTienda.value = data.rows[0].nombre_establecimiento;
+                direccionTienda.value = data.rows[0].direccion;
+                descripcionTienda.value = data.rows[0].descripcion;
+                document.getElementById("shop-name").value = data.rows[0].nombre_establecimiento;
+                document.getElementById("shop-address").value = data.rows[0].direccion;
+                document.getElementById("shop-desc").value = data.rows[0].descripcion;
+
+                document.getElementById("shop-name-preview").innerHTML = data.rows[0].nombre_establecimiento;
+                document.getElementById("location-preview").innerHTML = data.rows[0].direccion;
+                document.getElementById("description-preview").innerHTML = data.rows[0].descripcion;
+
+            }
+        })
+
+        .catch(error => console.error(error));
+}
+
+//actualizacion en tiempo real 
+let timer, timer1, timer2
 tituloTienda.addEventListener("input", () => {
     clearTimeout(timer)
     timer = setTimeout(() => {
@@ -16,8 +52,8 @@ tituloTienda.addEventListener("input", () => {
 });
 
 direccionTienda.addEventListener("input", () => {
-    clearTimeout(timer)
-    timer = setTimeout(() => {
+    clearTimeout(timer1)
+    timer1 = setTimeout(() => {
         if (direccionTienda.value === "") {
             document.getElementById("location-preview").innerHTML = "Calle de la Innovación 25, Colombia";
         } else {
@@ -29,8 +65,8 @@ direccionTienda.addEventListener("input", () => {
 });
 
 descripcionTienda.addEventListener("input", () => {
-    clearTimeout(timer)
-    timer = setTimeout(() => {
+    clearTimeout(timer2)
+    timer2 = setTimeout(() => {
         if (descripcionTienda.value === "") {
             document.getElementById("description-preview").innerHTML = "Optimiza tu tiempo y elimina las perdidas. Concéntrate en lo que más importa y deja que HoraLista se encargue de tu agenda.";
         } else {
@@ -41,3 +77,31 @@ descripcionTienda.addEventListener("input", () => {
     }, 1000);
 });
 
+
+//Guardar cambio 
+
+document.getElementById("btn-guardar-cambios").addEventListener("click", () => {
+
+    if (tituloTienda.value === "" || direccionTienda.value === "" || descripcionTienda.value === "") {
+        alertaMal("Todos los campos son obligatorios");
+        return;
+    }
+
+    console.log({ idfront: sessionStorage.getItem("Id"), tituloTienda: tituloTienda.value, direccionTienda: direccionTienda.value, descripcionTienda: descripcionTienda.value });
+    fetch(`${ruta}/api/tienda/identidad`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            idfront: sessionStorage.getItem("Id"),
+            nombre: tituloTienda.value,
+            direccion: direccionTienda.value,
+            descripcion: descripcionTienda.value
+        })
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+});
